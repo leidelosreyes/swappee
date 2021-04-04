498f3dd3f9b1777ae9e77ed210302f60c86bc1b3
leo dela cruz <leidelosreyes@github.com>
2021-04-04 23:17:05 +0800

    Adjust modal

--- resources/views/posts/view.blade.php
+++ resources/views/posts/view.blade.php
@@ -51,7 +51,7 @@
 
 <!-- Modal -->
 <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
+  <div class="modal-dialog modal-dialog-centered" role="document">
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title" id="exampleModalLabel">Message</h5>
@@ -91,7 +91,7 @@
 									 
 									<div class="modal-footer">
 									
-									<button type="button" class="btn btn-outline-danger" data-dismiss="modal"><i class="fas fa-times"></i></button>
+								
 									<button type="sumbit" class="btn btn-primary"style="background-color:#FFB52E;border:none;"> <i class="far fa-paper-plane"></i> Send</button>
                     </div>
 							    </form>
