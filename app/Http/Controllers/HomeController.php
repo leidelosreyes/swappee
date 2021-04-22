ff528da3f4176fd76f92c27e0849ea9f11a0389e
leidelosreyes <79086977+leidelosreyes@users.noreply.github.com>
2021-04-22 16:44:15 +0800

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\{User,offer,post,Categories,Sub_categorie};
+use App\Models\{User,Offer,Post,Categories,Sub_categorie};
 use Auth;
 
 use Carbon\Carbon;
