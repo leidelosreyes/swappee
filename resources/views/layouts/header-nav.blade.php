17224e2f392ee743571784d60dc28a93f65d25d3
leo dela cruz <leidelosreyes@github.com>
2021-04-23 14:07:04 +0800

    update

--- resources/views/layouts/header-nav.blade.php
+++ resources/views/layouts/header-nav.blade.php
@@ -65,8 +65,8 @@
 					<li class="dropdown">
 						<a href="" class="nav-item nav-link  navfonts" data-toggle="dropdown" style="color: #57585a;" ><i class="far fa-user-circle"></i> {{ Auth::user()->name }}</a>
 						<div class="dropdown-menu">
-							<a href="{{route('user.profile')}}" style="font-size: .80rem;" class="dropdown-item"><i class="fas fa-user"></i> Acount</a>
-                            <a href="{{route('user.profile_public_view')}}" style="font-size: .80rem;" class="dropdown-item"><i class="fas fa-globe-asia"></i> Public Profile</a>
+							<a href="{{route('User.profile')}}" style="font-size: .80rem;" class="dropdown-item"><i class="fas fa-user"></i> Acount</a>
+                            <a href="{{route('User.profile_public_view')}}" style="font-size: .80rem;" class="dropdown-item"><i class="fas fa-globe-asia"></i> Public Profile</a>
 							<a class="dropdown-item navfonts" href="{{ route('index') }}" style="font-size: .80rem;"
                             onclick="event.preventDefault();
                                             document.getElementById('logout-form').submit();">
@@ -151,7 +151,7 @@
 					<a class="nav-link" style="color: #57585a; font-size:20px;" href="{{route('auctions.index')}}"><i class="fas fa-gavel"></i></a>
 					</li>
 					<li class="nav-item">
-						<a href="{{route('user.profile')}}" class="nav-item nav-link  navfonts"  style="color: #57585a; font-size:20px;" ><i class="far fa-user-circle"></i></a>
+						<a href="{{route('User.profile')}}" class="nav-item nav-link  navfonts"  style="color: #57585a; font-size:20px;" ><i class="far fa-user-circle"></i></a>
 					</li>
 
 				</ul>
