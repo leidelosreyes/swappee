8d15543ded1da4dffc6384361e46dfaa5e5a43d7
leo dela cruz <leidelosreyes@github.com>
2021-04-28 01:43:46 +0800

    updated

--- resources/views/slider/slider.blade.php
+++ resources/views/slider/slider.blade.php
@@ -1,7 +1,7 @@
 
 
 <div class="col-sm-12 d-none d-lg-block">
-    <div class="slickSlider container ">
+    <div class="slickSlider">
             @foreach($categories as $category)
                 <div class="col-sm-1"><a href="{{route('filter.category',$category->id)}}" value="{{$category->id}}" style="border:none;background:none;outline:none !important;outline:0px;">
                     <img src="/storage/{{$category->icon}}" class="categories-radius" style="width:90px;"/></a>
@@ -12,7 +12,7 @@
 </div>
 
 <div class="col-sm-12 d-sm-block d-md-none">
-    <div class="slickSlider container  card-body">
+    <div class="slickSlider">
             @foreach($categories as $category)
                 <div class="col-sm-1"><a href="{{route('filter.category',$category->id)}}" value="{{$category->id}}" style="border:none;background:none;outline:none !important;outline:0px;">
                     <img src="/storage/{{$category->icon}}" class="categories-radius" style="width:65px;"/></a>
