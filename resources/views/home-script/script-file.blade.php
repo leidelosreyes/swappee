ffc8e8eb8e2a34a655333f09b7d170a648228e60
leo dela cruz <leidelosreyes@github.com>
2021-04-28 01:35:22 +0800

    Removed arrow

--- resources/views/home-script/script-file.blade.php
+++ resources/views/home-script/script-file.blade.php
@@ -51,8 +51,8 @@
   speed: 300,
   slidesToShow: 7,
   slidesToScroll: 5,
-  // prevArrow: null,
-  // nextArrow: null,
+  prevArrow: null,
+  nextArrow: null,
   responsive: [
     {
       breakpoint: 1024,
--- resources/views/layouts/layout1.blade.php
+++ resources/views/layouts/layout1.blade.php
@@ -131,19 +131,21 @@
     <!-- <script src="{{asset('js/index.js')}}"></script> -->
     <script type="text/javascript">
     $('.slickSlider').slick({
-  autoplay: true,
+      autoplay: true,
   autoplaySpeed: 2000,
   dots: true,
   infinite: true,
   speed: 300,
   slidesToShow: 7,
   slidesToScroll: 5,
+  prevArrow: null,
+  nextArrow: null,
   responsive: [
     {
       breakpoint: 1024,
       settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
+        slidesToShow: 5,
+        slidesToScroll: 5,
         infinite: true,
         dots: true
       }
@@ -151,15 +153,15 @@
     {
       breakpoint: 600,
       settings: {
-        slidesToShow: 4,
-        slidesToScroll: 2
+        slidesToShow: 5,
+        slidesToScroll: 5
       }
     },
     {
       breakpoint: 480,
       settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3
+        slidesToShow: 5,
+        slidesToScroll: 5
       }
     }
    
