9da282ac7ad4adda5deb86022e7a6592d9d76c94
leo dela cruz <leidelosreyes@github.com>
2021-04-28 16:36:04 +0800

    Added fav-icon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,7 @@
     <script src="{{ asset('js/app.js') }}" defer></script>
 
     <!-- Fonts -->
-     <link rel="shortcut icon" href="{{ asset('image/swappee_favicon.png') }}">
+    <link rel="shortcut icon" href="{{ asset('image/swap_trans.png') }}">
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
--- resources/views/layouts/layout2.blade.php
+++ resources/views/layouts/layout2.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ env('APP_NAME') }}</title>
-    <link rel="shortcut icon" href="{{ asset('image/swappee_favicon.png') }}">
+    <link rel="shortcut icon" href="{{ asset('image/swap_trans.png') }}">
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <link rel="stylesheet" href="{{asset('css/animate.css')}}">
    <!-- Add the slick-theme.css if you want default styling -->
