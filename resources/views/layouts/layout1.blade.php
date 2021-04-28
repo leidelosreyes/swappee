bdb790eef90d0f77ac9f35ae4fe6dae9c54d6488
leo dela cruz <leidelosreyes@github.com>
2021-04-28 14:53:05 +0800

    remove mobile view carousel

--- resources/views/layouts/layout1.blade.php
+++ resources/views/layouts/layout1.blade.php
@@ -144,8 +144,8 @@
     {
       breakpoint: 1024,
       settings: {
-        slidesToShow: 5,
-        slidesToScroll: 5,
+        slidesToShow: 4,
+        slidesToScroll: 4,
         infinite: true,
         dots: true
       }
@@ -153,15 +153,15 @@
     {
       breakpoint: 600,
       settings: {
-        slidesToShow: 5,
-        slidesToScroll: 5
+        slidesToShow: 4,
+        slidesToScroll: 4
       }
     },
     {
       breakpoint: 480,
       settings: {
-        slidesToShow: 5,
-        slidesToScroll: 5
+        slidesToShow: 4,
+        slidesToScroll: 4
       }
     }
    
--- resources/views/posts/view.blade.php
+++ resources/views/posts/view.blade.php
@@ -1,7 +1,68 @@
 @extends('layouts.layout2')
 @section('content')
 <div class="container">
-@include('layouts.carousel')
+   <!-- carousel for web view  -->
+
+<div class="d-none d-lg-block">
+  <div id="carouselExampleIndicators" class="carousel slide " data-ride="carousel">
+    <ol class="carousel-indicators">
+      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner">
+      <div class="carousel-item active">    
+              <img class="carousel-image" style="height:400px; width:100%; object-fit:cover; " src="{{asset('image/delivery-c-new.jpg')}}" alt="First slide">
+              <div class="carousel-caption d-md-block">
+              <h1 style="font-family:Copperplate; font-size: 40px;">
+                  <span>Swappee</span>
+              </h1>
+            
+              <h3 style="color: #FE980F;font-family: 'Roboto', sans-serif;font-size: 28px;font-weight: 700">Cash Less Transactions for Swap!</h3>
+            
+              
+              <p style="color: #ffffff;font-size: 16px;font-weight: 300;font-family: 'Roboto', sans-serif;margin-top: 15px;">Swapping will make you fell better. </p>
+        </div>
+      </div>
+      <div class="carousel-item ">
+        <img class="carousel-image" style="height:400px; width:100%; " src="{{asset('image/new-swap-c.jpg')}}" alt="Second slide">
+        <div class="carousel-caption d-md-block">
+              <h1 style="font-family:Copperplate; font-size: 40px;">
+                  <span>Deliver</span>
+              </h1>
+              <h3 style="color: #FE980F;font-family: 'Roboto', sans-serif;font-size: 28px;font-weight: 700;margin-top: 15px;">Cash Less Transactions for Swap!</h3>
+              
+            
+              <p style="color: #ffffff;font-size: 16px;font-weight: 300;font-family: 'Roboto', sans-serif;margin-top: 15px;">We deliver your package with love. </p>
+
+        </div>
+      
+      </div>
+      <div class="carousel-item ">
+        <img class="carousel-image" style="height:400px; width:100%; " src="{{asset('image/bidding new.jpg')}}" alt="Third slide">
+        <div class="carousel-caption d-md-block">
+              <h1 style="font-family:Copperplate; font-size: 40px;">
+                  <span>Auction</span>
+              </h1>
+              <h3  style="color: #FE980F;font-family: 'Roboto', sans-serif;font-size: 28px;font-weight: 700">Cash Less Transactions for Swap!</h3>
+              <p style="color: #ffffff;font-size: 16px;font-weight: 300;font-family: 'Roboto', sans-serif;margin-top: 15px;">Our auction is fair and competitive, enter at your own risk. </p>
+
+        </div>
+      
+      </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+ </div>
+</div>
+<!-- end carousel for web view  -->
+    
 </div>
 <div class="container">
 	<div class="row">
