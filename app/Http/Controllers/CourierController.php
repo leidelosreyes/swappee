57040c7add8333dea3e579c8e058e0b7655641b6
leo dela cruz <leidelosreyes@github.com>
2021-05-09 05:19:45 +0800

    Make Function for Calculation of locations

--- app/Http/Controllers/CourierController.php
+++ app/Http/Controllers/CourierController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\{bidder,auction,Offer,Message,User};
 use Auth;
 use Mail;
 use App\Models\Profile;
@@ -10,8 +11,15 @@ use App\Models\Profile;
 class CourierController extends Controller
 {
     public function calculate_order(){
+        $auction_id= bidder::where('user_id',Auth::id())->Where('winners',1)->first();
+        $id = $auction_id->auction_id;
+        $find_owner = auction::where('id',$id)->first();
+        $owner_id = $find_owner->user_id;
+        $owner = User::findOrFail($owner_id);
+        $pickup_location = $owner->profile->address;
+        $contact_no =  $owner->profile->cellphone_no;
         $curl = curl_init(); 
-        curl_setopt($curl, CURLOPT_URL, 'https://robotapitest.mrspeedy.ph/api/business/1.1/create-order'); 
+        curl_setopt($curl, CURLOPT_URL, 'https://robotapitest.mrspeedy.ph/api/business/1.1/calculate-order'); 
         curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'POST'); 
         curl_setopt($curl, CURLOPT_HTTPHEADER, ['X-DV-Auth-Token: 3AA731EB88E2F1155562E4CBFA059C8D00C20959']); 
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
@@ -20,15 +28,64 @@ class CourierController extends Controller
             'matter' => 'Documents', 
             'points' => [ 
                 [ 
-                    'address' => 'camarin caloocan city', 
+                    'address' => $pickup_location, 
+                ], 
+                [ 
+                    'address' => $request->drop_off_location, 
+                ], 
+            ], 
+        ]; 
+         
+        $json = json_encode($data, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES); 
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $json); 
+         
+        $result = curl_exec($curl); 
+        if ($result === false) { 
+            throw new Exception(curl_error($curl), curl_errno($curl)); 
+        } 
+         
+        $object = json_decode($result);
+        dd($object);
+        echo $object->order->points['0']->address;
+        echo '<br>';
+        echo $object->order->points['1']->address;
+        echo '<br>';
+      
+        echo '<br>';
+        echo $object->order->points['0']->tracking_url;
+        echo '<br>';
+        echo $object->order->delivery_fee_amount;
+        echo $object->order->status;
+        
+    }
+
+    public function store(Request $request){
+        $auction_id= bidder::where('user_id',Auth::id())->Where('winners',1)->first();
+        $id = $auction_id->auction_id;
+        $find_owner = auction::where('id',$id)->first();
+        $owner_id = $find_owner->user_id;
+        $owner = User::findOrFail($owner_id);
+        $pickup_location = $owner->profile->address;
+        $contact_no =  $owner->profile->cellphone_no;
+        $curl = curl_init(); 
+        curl_setopt($curl, CURLOPT_URL, 'https://robotapitest.mrspeedy.ph/api/business/1.1/calculate-order'); 
+        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'POST'); 
+        curl_setopt($curl, CURLOPT_HTTPHEADER, ['X-DV-Auth-Token: 3AA731EB88E2F1155562E4CBFA059C8D00C20959']); 
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        $profile = Profile::where('user_id',Auth::id())->first();
+        $data = [ 
+            'matter' => 'Documents', 
+            'points' => [ 
+                [ 
+                    'address' => $pickup_location, 
                     'contact_person' => [ 
-                        'phone' => '639195380326', 
+                        'phone' => $contact_no, 
                     ], 
                 ], 
                 [ 
-                    'address' => 'Unit 802, EcoTower, 32nd Street corner 9th Avenue, BGC, Taguig, 1634 Metro Manila', 
+                    'address' =>   $request->drop_off_location, 
                     'contact_person' => [ 
-                        'phone' => '639195380326', 
+                        'phone' =>$request->contact_number, 
                     ], 
                 ], 
             ], 
@@ -43,7 +100,7 @@ class CourierController extends Controller
         } 
          
         $object = json_decode($result);
-        
+    
         echo $object->order->points['0']->address;
         echo '<br>';
         echo $object->order->points['1']->address;
@@ -56,7 +113,6 @@ class CourierController extends Controller
         echo $object->order->status;
         
     }
-
     public function courier_info_and_location()
     {
         $body_message = "You are the winner of the bidding Please Go to swappee and buy the Cost of item and delivery Charge then give your Drop off location and Contact Person to Deliver your item" ;
