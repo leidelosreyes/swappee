c613bab287aa35e50c28fcbfe07d14322b61a125
Gilbert <gbhert09@gmail.com>
2021-05-10 05:20:55 +0800

    Changes in table-header , created approval and delete btn

--- resources/views/admins/admin/swap/show.blade.php
+++ resources/views/admins/admin/swap/show.blade.php
@@ -18,31 +18,48 @@
                                 <table class="table table-hover">
   <thead>
     <tr>
-    
-      <th scope="col">Name</th>
-      <th scope="col">Price</th>
-      <th scope="col">Location</th>
-      <th scope="col">Approved</th>
-      <th scope="col">Actions</th>
+      <th scope="col">Username</th>
+      <th scope="col">Item Name</th>
+      <th scope="col">Item Price</th>
+      <th scope="col">User Address</th>
+      <th scope="col" style="padding-left:30px">Item Approval</th>
+      <th scope="col" style="padding-left:30px;">Item Status</th>
+      <th scope="col" style="padding-left:25px;">Actions</th>
     </tr>
   </thead>
   <tbody>
   
     <tr>
     @foreach($swap as $swap)
-     
+      <td> <p class="mt-2">{{$swap->user->name}}</p> </td>
       <td> <p class="mt-2">{{$swap->product_name}}</p> </td>
       <td> <p class="mt-2">{{$swap->price}}</p> </td>
       <td> <p class="mt-2">{{$swap->location}}</p> </td>
-      <td>  {{$swap->approved}} </td>
       <td>
-                                    <div class="d-flex mt-2">
-                                    <a href="#"><i class="far fa-eye" style="color: #57585a; margin-right:5px;"></i>
-                                    <a href="#" style="margin-right:5px;"><i class="fas fa-edit"></i></a>
+        {!! Form::open(['route' => ['post_approval.admin', $swap->id], 'method' => 'post']) !!}
+            <button class="btn btn-success btn-sm" style="border:1px;">
+            <i class="fas fa-check" style="color:white;margin-left:10px;padding-right:10px;">Approve</i>
+            </button>     
+        {!!Form::close() !!}
+      </td>
+      <td style="padding-left:20px;">  
+        
+        @if($swap->approved == true)
+        <span class="bg-blue">Approved</span>
+        @else
+        <i class="fas fa-history" style="color:red;"></i> <span class="bg-pink">Pending</span>
+        @endif
+      </td>
+      <td>
+                                    <div class="d-flex">
+                                    <a href="#"><i class="far fa-eye" style="color: #57585a; margin-right:5px;padding-right:10px"></i>
+                                    <a href="#" style="margin-right:5px;padding-right:10px"><i class="fas fa-edit"></i></a>
                                    
-                                            <button class="delete" style="border:none;background:none;outline:none !important;outline:0px;">
-                                                <i class="fas fa-trash-alt" style="color:red;"></i>
-                                            </button>
+                                    {!! Form::open(['route' => ['delete_swap.admin', $swap->id], 'method' => 'get']) !!}
+                                      <button class="delete" style="border:none;background:none;outline:none !important;outline:0px;">
+                                      <i class="fas fa-trash-alt" style="color:red;"></i>
+                                      </button>     
+                                    {!!Form::close() !!}
                                 
                                     </div>
                                     </td>
@@ -56,5 +73,6 @@
         </div>
     </div>
 </div>
+
 
 @endsection
