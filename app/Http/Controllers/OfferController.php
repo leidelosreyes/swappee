a89a47a1894444c23a5e1586212353eb4a957c9f
Gilbert <gbhert09@gmail.com>
2021-05-10 19:03:32 +0800

    Make notification for swapping

--- app/Http/Controllers/OfferController.php
+++ app/Http/Controllers/OfferController.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 use App\Models\Post;
 use App\Models\Offer;
+use Mail;
 use App\Models\Message;
 use App\Models\User;
 use Auth;
@@ -41,9 +42,8 @@ class OfferController extends Controller
    }
    public function store(Request $request )
    {
-
-
-    $data = request()->validate
+      
+         $data = request()->validate
        ([
         'product_name' => 'required',
         'description' => 'required',
@@ -55,6 +55,7 @@ class OfferController extends Controller
         'image' => 'required|max:8192|image'
 
        ]);
+      
        $imagePath = request('image')->store('uploads','public');
     auth()->user()->sender()->create
     ([
@@ -67,7 +68,20 @@ class OfferController extends Controller
     'post_id' => $data['post_id'],
     'image' => $imagePath
        ]);
-
+       //-------------------------- for sending notifications -------------------------//
+       $find_receiver_id = $request->receiver_id;
+       $receiver= User::findOrFail($find_receiver_id);
+       $receiver_name = $receiver->name;
+       $user_email = $receiver->email;
+       $sender_name = Auth::user()->name;
+       $body_message = "{$sender_name} makes an offer to your item; \n item offered name : {$request->product_name} \n Clink the link to redirect to Swappee to see full details";
+       $subject= "Congratulations You Are The Winner";
+       $data = array('name'=>$receiver_name, "body" => $body_message);
+       Mail::send('messages.mail', $data, function($message) use($user_email) {
+       $message->to($user_email)->subject('For Swap');
+       $message->from('swappee6@gmail.com','Swappee');
+       });
+       
        return redirect('home')->with('success','Please wait to accept your offer');
    }
 }
