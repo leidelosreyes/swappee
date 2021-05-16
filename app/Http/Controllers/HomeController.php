ed9a4b94cdabd02246baedc07455e050bf191ba3
Rey Mark <poknaitz@gmail.com>
2021-05-16 04:41:18 -0700

    integrating activity log functionality

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\{User,Offer,Post,Categories,Sub_categorie,Message};
+use App\Models\{User,Offer,Post,Categories,Sub_categorie,Message,ActivityLog};
 use Auth;
 
 use Carbon\Carbon;
