512a2451e0e65dff60833653443e6c079cf048dd
leo dela cruz <leidelosreyes@github.com>
2021-06-09 00:37:33 +0800

    change code structure of getting points

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -23,7 +23,7 @@ class ProfileController extends Controller
     $messages = Message::where('receiver_id',Auth::id())->get();
     $offer = Offer::where('sender_id',Auth::id())->get();
     $notifications = offer::where('receiver_id',Auth::id())->where('is_accepted',0)->get();
-    $points = Point::where('user_id',Auth::id())->first();
+    $points = Auth::user()->points->amount;
     return view('User.profile', compact('posts','messages','notifications','offer','points'));
    }
    public function auction_index()
