abc8a9183fb14ec3a8e92212ea6a86c85c5b8429
leo dela cruz <leidelosreyes@github.com>
2021-06-09 01:09:32 +0800

    updated

--- app/Models/Point.php
+++ app/Models/Point.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Point extends Model
 {
     use HasFactory;
-    protected $fillable = ['user_id','amount'];
+    protected $guarded=[];
 
     public function user(){
         return $this->belongsTo(User::class);
