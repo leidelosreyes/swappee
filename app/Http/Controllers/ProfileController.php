0b99493b380549e5e838cd68dd4551f075fff3f4
leo dela cruz <leidelosreyes@github.com>
2021-06-09 01:04:49 +0800

    updated

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -23,7 +23,7 @@ class ProfileController extends Controller
     $messages = Message::where('receiver_id',Auth::id())->get();
     $offer = Offer::where('sender_id',Auth::id())->get();
      $notifications = offer::where('receiver_id',Auth::id())->where('is_accepted',0)->get();
-    $points = Point::where('user_id',Auth::id())->firstOrFail();
+    $points = Point::where('user_id',Auth::id())->first();
     return view('User.profile', compact('posts','messages','notifications','offer','points'));
    }
    public function auction_index()
